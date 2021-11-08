6c3a15b29ea71af1eab1f4d7bb83695c17a9675b
afcking <owner@afcking.com>
2021-11-08 05:23:58 -0600

    8st Nov 2021.log in page

--- lib/log_in.dart
+++ lib/log_in.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+String _email= "tamgidhossen2003@gmail.com";
+String _password= "heydxcat";
+final _formKey=GlobalKey<FormState>();
+
+
 class LogIn extends StatefulWidget {
   const LogIn({Key? key}) : super(key: key);
 
@@ -17,21 +22,74 @@ class _LogInState extends State<LogIn> {
       body:Container(
         width: MediaQuery.of(context).size.width,
         color: Colors.white,
-        child:  Column(
-          children: [
-            Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
+        child:  Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
 
-                  image: DecorationImage(
-                      image: AssetImage("assets/images/image4.png")
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/image4.png")
 
+                        ),
+                        borderRadius: BorderRadius.circular(200)
+
+                    ),
                   ),
-                  borderRadius: BorderRadius.circular(200)
+                  SizedBox(height: 24,),
+                  TextFormField(
+                    validator: (text){
+                      if(text==null|| text.isEmpty){
+                        return "This field is empty";
+                      }
+                    },
+                    cursorColor: Colors.black,
+                     decoration: InputDecoration(
+                       suffix: Icon(Icons.attach_email),
+                       hintText: "Enter Your Email",
+                       focusedBorder: OutlineInputBorder(
+                         borderRadius: BorderRadius.circular(8),
+                         borderSide: BorderSide(color: Colors.black),
+                       ),
+                     ),
+                  ),
+                  SizedBox(
+                      height: 20),
+                  TextFormField(
+                    validator: (text){
+                      if(text==null|| text.isEmpty){
+                        return "This field is empty";
+                      }
+                    },
+                    obscureText: true,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      suffix: Icon(Icons.access_time_filled),
+                      hintText: "Enter Your Password",
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                      height: 20),
+                  ElevatedButton(
+                      onPressed: (){
+                        _formKey.currentState!.validate();
+                      },
+                      child: Text("Log In")
+                  )
+                ],
               ),
-            )
-          ],
+            ),
+          ),
         ),
       )
     );
---
+++ lib/sign_up.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+String _email="dxcat";
+String _password="dxjui";
+final _formKey=GlobalKey<FormState>();
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       title: Text("Sign Up Page"),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    validator: (text){
+                      if(text==null ||text.isEmpty ){
+                        return"This field is empty";
+                      }
+                    },
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      suffix: Icon(Icons.attach_email_sharp),
+                      hintText: ("Enter Your Email"),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.black),
+                      )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    validator: (text){
+                      if(text==null ||text.isEmpty ){
+                        return"This field is empty";
+                      }
+                    },
+                    obscureText: true,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        suffix: Icon(Icons.remove_red_eye),
+                        hintText: ("Enter Your Password"),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black),
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
